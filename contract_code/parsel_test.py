29d2e5a05b5b5f3f7379287ed9e1ec82ecdad0ab
Evgeny Medvedev <evgeny@kitchenup.com>
2018-02-25 14:13:15 +0700

    Refactor

--- contract_code/parsel_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-from parsel import Selector
-url = 'https://etherscan.io/address/0x75f9379833800f397ead23dc2317daa456a4bedb#code'
-text = requests.get(url).text
-selector = Selector(text=text)
-print(selector.css('#editor::text').extract())
